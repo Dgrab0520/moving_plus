35d97f247e04dc548926a43e1874df16ca8b0e9a
TodaudRur <skstjdtndi01@naver.com>
2022-02-25 13:18:27 +0900

    0225 plus final release setting

--- lib/pages/p_signup.dart
+++ lib/pages/p_signup.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:moving_plus/pages/main_page.dart';
 
-
 import '../widgets/p_login.dart';
 
 class P_Signup extends StatefulWidget {
@@ -16,255 +15,279 @@ class _P_SignupState extends State<P_Signup> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      shape:RoundedRectangleBorder(
+      shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(
-            Radius.circular(32),
-          )
-      ),
+        Radius.circular(32),
+      )),
       backgroundColor: Colors.white,
-      content:SingleChildScrollView(
+      content: SingleChildScrollView(
         child: Container(
-          width:MediaQuery.of(context).size.width,
+          width: MediaQuery.of(context).size.width,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Align(
                 alignment: Alignment.centerLeft,
                 child: InkWell(
-                    onTap:(){
+                    onTap: () {
                       Get.back();
                       Get.dialog(P_Login());
                     },
-                    child: Container(
-                        child: Icon(Icons.close)
-                    )
-                ),
+                    child: Container(child: Icon(Icons.close))),
               ),
-
-              SizedBox(height:10),
-              Text('파트너 회원가입',
-                style:TextStyle(
-                  color:Color(0xFF444444),
-                  fontSize:23,
+              SizedBox(height: 10),
+              Text(
+                '파트너 회원가입',
+                style: TextStyle(
+                  color: Color(0xFF444444),
+                  fontSize: 23,
                   fontFamily: 'NanumSquareB',
                 ),
               ),
-              SizedBox(height:7),
-              Text('로그인 후 입주 플러스를 이용해 주세요.',
-                style:TextStyle(
-                  color:Color(0xFF444444),
-                  fontSize:12,
+              SizedBox(height: 7),
+              Text(
+                '로그인 후 입주 플러스를 이용해 주세요.',
+                style: TextStyle(
+                  color: Color(0xFF444444),
+                  fontSize: 12,
                   fontFamily: 'NanumSquareR',
                 ),
               ),
-              SizedBox(height:28),
+              SizedBox(height: 28),
               Container(
-                padding: EdgeInsets.only(left:8,right:8),
+                padding: EdgeInsets.only(left: 8, right: 8),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('아이디',
-                      style:TextStyle(
-                        color:Color(0xFF444444),
-                        fontSize:12,
+                    Text(
+                      '아이디',
+                      style: TextStyle(
+                        color: Color(0xFF444444),
+                        fontSize: 12,
                         fontFamily: 'NanumSquareR',
                       ),
                     ),
-                    SizedBox(height:7),
+                    SizedBox(height: 7),
                     Container(
-                        width:double.infinity,
-                        height:45,
+                        width: double.infinity,
+                        height: 45,
                         child: TextField(
                           decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(top:10.0,bottom:10,left:15),
+                            contentPadding: EdgeInsets.only(
+                                top: 10.0, bottom: 10, left: 15),
                             counterStyle: TextStyle(
-                              fontSize:10,
+                              fontSize: 10,
                             ),
                             hintStyle: TextStyle(
-                              fontSize:10,
+                              fontSize: 10,
                             ),
                             hintText: '아이디 이메일을 입력해주세요.',
                             labelStyle: TextStyle(color: Color(0xFFACACAC)),
                             focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                              borderSide: BorderSide(width: 1, color:  Color(0xFFACACAC)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5.0)),
+                              borderSide: BorderSide(
+                                  width: 1, color: Color(0xFFACACAC)),
                             ),
                             enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                              borderSide: BorderSide(width: 1, color:  Color(0xFFACACAC)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5.0)),
+                              borderSide: BorderSide(
+                                  width: 1, color: Color(0xFFACACAC)),
                             ),
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5.0)),
                             ),
                           ),
                           keyboardType: TextInputType.emailAddress,
-                        )
-                    ),
-                    SizedBox(height:20),
+                        )),
+                    SizedBox(height: 20),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('비밀번호',
-                          style:TextStyle(
-                            color:Color(0xFF444444),
-                            fontSize:12,
+                        Text(
+                          '비밀번호',
+                          style: TextStyle(
+                            color: Color(0xFF444444),
+                            fontSize: 12,
                             fontFamily: 'NanumSquareR',
                           ),
                         ),
-                        SizedBox(height:7),
+                        SizedBox(height: 7),
                         Container(
-                            width:double.infinity,
-                            height:45,
+                            width: double.infinity,
+                            height: 45,
                             child: TextField(
                               decoration: InputDecoration(
-                                contentPadding: EdgeInsets.only(top:10.0,bottom:10,left:15),
+                                contentPadding: EdgeInsets.only(
+                                    top: 10.0, bottom: 10, left: 15),
                                 counterStyle: TextStyle(
-                                  fontSize:10,
+                                  fontSize: 10,
                                 ),
                                 hintStyle: TextStyle(
-                                  fontSize:10,
+                                  fontSize: 10,
                                 ),
                                 hintText: '비밀번호를 입력해주세요.',
                                 labelStyle: TextStyle(color: Color(0xFFACACAC)),
                                 focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                                  borderSide: BorderSide(width: 1, color:  Color(0xFFACACAC)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  borderSide: BorderSide(
+                                      width: 1, color: Color(0xFFACACAC)),
                                 ),
                                 enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                                  borderSide: BorderSide(width: 1, color:  Color(0xFFACACAC)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  borderSide: BorderSide(
+                                      width: 1, color: Color(0xFFACACAC)),
                                 ),
                                 border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
                                 ),
                               ),
                               keyboardType: TextInputType.visiblePassword,
-                            )
-                        ),
+                            )),
                       ],
                     ),
-                    SizedBox(height:20),
+                    SizedBox(height: 20),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('비밀번호 확인',
-                          style:TextStyle(
-                            color:Color(0xFF444444),
-                            fontSize:12,
+                        Text(
+                          '비밀번호 확인',
+                          style: TextStyle(
+                            color: Color(0xFF444444),
+                            fontSize: 12,
                             fontFamily: 'NanumSquareR',
                           ),
                         ),
-                        SizedBox(height:7),
+                        SizedBox(height: 7),
                         Container(
-
-                            width:double.infinity,
-                            height:45,
+                            width: double.infinity,
+                            height: 45,
                             child: TextField(
                               decoration: InputDecoration(
-                                contentPadding: EdgeInsets.only(top:10.0,bottom:10,left:15),
+                                contentPadding: EdgeInsets.only(
+                                    top: 10.0, bottom: 10, left: 15),
                                 counterStyle: TextStyle(
-                                  fontSize:10,
+                                  fontSize: 10,
                                 ),
                                 hintStyle: TextStyle(
-                                  fontSize:10,
+                                  fontSize: 10,
                                 ),
                                 hintText: '비밀번호를 한번 더 입력해주세요.',
                                 labelStyle: TextStyle(color: Color(0xFFACACAC)),
                                 focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                                  borderSide: BorderSide(width: 1, color:  Color(0xFFACACAC)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  borderSide: BorderSide(
+                                      width: 1, color: Color(0xFFACACAC)),
                                 ),
                                 enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                                  borderSide: BorderSide(width: 1, color:  Color(0xFFACACAC)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  borderSide: BorderSide(
+                                      width: 1, color: Color(0xFFACACAC)),
                                 ),
                                 border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
                                 ),
                               ),
                               keyboardType: TextInputType.visiblePassword,
-                            )
-                        ),
+                            )),
                       ],
                     ),
-                    SizedBox(height:20),
+                    SizedBox(height: 20),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('업체명',
-                          style:TextStyle(
-                            color:Color(0xFF444444),
-                            fontSize:12,
+                        Text(
+                          '업체명',
+                          style: TextStyle(
+                            color: Color(0xFF444444),
+                            fontSize: 12,
                             fontFamily: 'NanumSquareR',
                           ),
                         ),
-                        SizedBox(height:7),
+                        SizedBox(height: 7),
                         Container(
-                            width:double.infinity,
-                            height:45,
+                            width: double.infinity,
+                            height: 45,
                             child: TextField(
                               decoration: InputDecoration(
-                                contentPadding: EdgeInsets.only(top:10.0,bottom:10,left:15),
+                                contentPadding: EdgeInsets.only(
+                                    top: 10.0, bottom: 10, left: 15),
                                 counterStyle: TextStyle(
-                                  fontSize:10,
+                                  fontSize: 10,
                                 ),
                                 hintStyle: TextStyle(
-                                  fontSize:10,
+                                  fontSize: 10,
                                 ),
                                 hintText: '업체명을 입력해주세요.',
                                 labelStyle: TextStyle(color: Color(0xFFACACAC)),
                                 focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                                  borderSide: BorderSide(width: 1, color:  Color(0xFFACACAC)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  borderSide: BorderSide(
+                                      width: 1, color: Color(0xFFACACAC)),
                                 ),
                                 enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                                  borderSide: BorderSide(width: 1, color:  Color(0xFFACACAC)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  borderSide: BorderSide(
+                                      width: 1, color: Color(0xFFACACAC)),
                                 ),
                                 border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
                                 ),
                               ),
                               keyboardType: TextInputType.visiblePassword,
-                            )
-                        ),
+                            )),
                       ],
                     ),
                   ],
                 ),
               ),
-              SizedBox(height:30),
+              SizedBox(height: 30),
               InkWell(
-                onTap:(){
+                onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => Main_Page(index: 1,)),
+                    MaterialPageRoute(
+                        builder: (context) => Main_Page(
+                              index: 1,
+                            )),
                   );
                 },
                 child: Container(
-                  margin: EdgeInsets.only(left:8,right:8),
+                  margin: EdgeInsets.only(left: 8, right: 8),
                   width: double.infinity,
-                  height:50,
-                  decoration:BoxDecoration(
-                    color:Color(0xFF025595),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF025595),
                     borderRadius: BorderRadius.circular(5),
                   ),
                   child: Center(
-                    child: Text('가입하기',
+                    child: Text(
+                      '가입하기',
                       style: TextStyle(
-                        fontSize:15,
-                        color:Colors.white,
-                        fontFamily:'NanumSquareB',
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontFamily: 'NanumSquareB',
                       ),
                     ),
                   ),
                 ),
               ),
-              SizedBox(height:30),
+              SizedBox(height: 30),
             ],
           ),
         ),
